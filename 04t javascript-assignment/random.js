8145eee31d99dfff58092a12746da5333243bd4e
Kris Nolte <krisnolte@gmail.com>
2015-10-13 18:39:36 -0400

    d3 and magnetic fields homework

---
+++ 04t javascript-assignment/random.js
@@ -0,0 +1,13 @@
+<script>
+	var fruit = [
+                "apple",
+                "banana",
+                "orange",
+                "grape",
+                "lime",
+                "lemon", 
+                "kiwi"
+            ];
+
+    var randomfruit = document.write("Here's a random fruit: "+ fruit[Math.floor(Math.random()*fruit.length - 1) - 0 + 1))]);
+</script>
